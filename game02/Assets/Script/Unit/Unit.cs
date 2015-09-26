4e4796f4492e8d756ef3affd923e119afd3e8192
sisui <sainfrau@yahoo.co.jp>
2015-09-26 18:24:12 +0900

    ユニットクラスを変更

--- game02/Assets/Script/Unit/Unit.cs
+++ game02/Assets/Script/Unit/Unit.cs
@@ -4,15 +4,26 @@ using System.Collections;
 /// <summary>
 /// ユニットの状態を保持するクラス。
 /// </summary>
-public class Unit : MonoBehaviour {
+public class Unit {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public string name { get; set; }
+    // 種族名
+    public string type_name { get; set; }
+    //種族属性　近接/遠距離/呪い
+    public int type { get; set; }
+    //陣営
+    public int ally { get; set; }
+    public int buff { get; set; }
+
+    public int lvl { get; set; }
+    public int exp { get; set; }
+
+    public int mobility { get; set; }
+    public int hp { get; set; }
+    public int atk { get; set; }
+    public int def { get; set; }
+    public int dod { get; set; }
+    public int hit { get; set; }
+
+
 }
