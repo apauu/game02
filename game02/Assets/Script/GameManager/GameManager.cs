462c681ecfba3ebc7a409ef1b2c4a30737849736
kaneda.naoki <redbull.nomitai@gmail.com>
2016-01-09 18:52:48 +0900

    コマンドメニューからボタン操作処理を作成

--- game02/Assets/Script/GameManager/GameManager.cs
+++ game02/Assets/Script/GameManager/GameManager.cs
@@ -6,10 +6,45 @@ using System.Collections;
 /// </summary>
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+
+
+    // Singlleton用プロパティ　インスタンスが存在するか？
+    static bool existsInstance = false;
+
+    // pricate プロパティ
+    private GameObject menu;
+    public GameObject menuCanvasPrefab;
+
+    // 初期化
+    void Awake()
+    {
+        // インスタンスが存在するなら破棄する
+        if (existsInstance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // 存在しない場合
+        // 自身が唯一のインスタンスとなる
+        existsInstance = true;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        try {
+            //プレハブ作成
+            menu = (GameObject)Instantiate(menuCanvasPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        
+            //初期表示自に不要なものは非表示にする
+            menu.SetActive(false);
+        }
+        catch (UnityException e){
+            Debug.Log(e);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,5 +55,20 @@ public class GameManager : MonoBehaviour {
     void ObserveWinLose()
     {
         
+    }
+
+    // キャラクターコマンドを実際の処理に割り振る
+    public void  CharacterCommandFacade(string processName)
+    {
+        //移動処理
+
+        //攻撃処理
+
+        //妖術処理
+
+        //装備処理
+
+
+        //Debug.Log(processName);
     }
 }
---
+++ game02/Assets/Script/Menu/characterCommandButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// キャラクターコマンドメニューのボタン押下に使用するクラス
+/// </summary>
+public class characterCommandButton : MonoBehaviour {
+
+    //private propaties
+    private GameObject gameManagerObject;
+    private GameManager gameManager;
+
+    //private GameManager gameManager;
+
+    // Use this for initialization
+    void Start () {
+        gameManagerObject = GameObject.Find("GameManagerPrefab");
+        gameManager = gameManagerObject.GetComponent<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// クリック時のイベント　処理はgameManagerに依頼
+    /// </summary>
+    public void OnClick () 
+    {
+        gameManager.CharacterCommandFacade(this.gameObject.name);
+    }
+}
